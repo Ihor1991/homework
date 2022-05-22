0ded4f07aae8e070e8b9ee2e79bbc61aef608a93
Ihor <romanovihor91@gmail.com>
2022-05-22 17:51:29 +0300

    first homework

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-let firstName = 5,
-    lastName = 5;
+let numberOfFilms = prompt('Сколько фильмов вы посмотрели?');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    genres: {},
+    actors: [],
+    privat: false
+};
 
-console.log(firstName !== lastName);
+let a = prompt('Один из последних просмотренных фильмов?'),
+    b = prompt('На сколько оцените его?'),
+    c = prompt('Один из последних просмотренных фильмов?'),
+    d = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
